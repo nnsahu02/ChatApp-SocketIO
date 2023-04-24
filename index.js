887ebea75a69a91cb302e959d02818ac268fe645
nnsahu02 <nnsahu2022@gmail.com>
2023-04-24 18:08:34 +0530

    -->Added chat functionality

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
+const server = app.listen(3000, () => { console.log('Server started on port 3000') });
 const io = require('socket.io')(server);
+
 
 // Serve static files from the public directory
 app.use(express.static('public'));
 
+
+let socketsConnected = new Set()
+
+
 // Handle incoming socket connections
-io.on('connection', socket => {
-    //console.log(`New client connected: ${socket.id}`);
+io.on('connection', onConnected);
 
-    // Listen for incoming chat messages
-    socket.on('chat message', message => {
-       // console.log(`New message from ${socket.id}: ${message}`);
+function onConnected(socket) {
+    console.log(`New client connected: ${socket.id}`);
+    socketsConnected.add(socket.id)
 
-        // Broadcast the message to all connected clients
-        io.emit('chat message', { id: socket.id, message });
-    });
-});
+    io.emit('clients-total', socketsConnected.size)
 
-// Start the server
-server.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
+    socket.on('disconnect', () => {
+        console.log(`socket disconnected ${socket.id}`)
+        socketsConnected.delete(socket.id)
+        io.emit('clients-total', socketsConnected.size)
+    })
+
+    socket.on('message', (data) => {
+        // console.log(data)
+        socket.broadcast.emit('chat-message', data)
+    })
+
+    socket.on('feedback', (data) => {
+        socket.broadcast.emit('feedback', data)
+    })
+}
+
+
