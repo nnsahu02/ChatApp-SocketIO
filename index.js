16a0e4eee007afe2c350e5f3689d9a4cd7b502e1
nnsahu <nnsahu2022@gmail.com>
2023-05-02 11:31:54 +0530

    -->Mongo integrated

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const server = app.listen(3000, () => { console.log('Server started on port 3000') });
-const io = require('socket.io')(server);
 
+const PORT = 3000;
+const server = app.listen(PORT, () => {
+    console.log("Server started on port 3000");
+});
+const io = require("socket.io")(server);
+
+const DB_URL =
+    "mongodb+srv://decobee:decobee2000@decobeedb.bqaczwd.mongodb.net/chatApp";
+mongoose
+    .connect(DB_URL)
+    .then(() => console.log(`MongoDB is connected.`))
+    .catch((err) => console.log(err));
 
 // Serve static files from the public directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-
-let socketsConnected = new Set()
-
+let socketsConnected = new Set();
 
 // Handle incoming socket connections
-io.on('connection', onConnected);
+io.on("connection", onConnected);
 
 function onConnected(socket) {
     console.log(`New client connected: ${socket.id}`);
-    socketsConnected.add(socket.id)
+    socketsConnected.add(socket.id);
 
-    io.emit('clients-total', socketsConnected.size)
+    io.emit("clients-total", socketsConnected.size);
 
-    socket.on('disconnect', () => {
-        console.log(`socket disconnected ${socket.id}`)
-        socketsConnected.delete(socket.id)
-        io.emit('clients-total', socketsConnected.size)
-    })
+    socket.on("disconnect", () => {
+        console.log(`socket disconnected ${socket.id}`);
+        socketsConnected.delete(socket.id);
+        io.emit("clients-total", socketsConnected.size);
+    });
 
-    socket.on('message', (data) => {
+    socket.on("message", (data) => {
         // console.log(data)
-        socket.broadcast.emit('chat-message', data)
-    })
+        socket.broadcast.emit("chat-message", data);
+    });
 
-    socket.on('feedback', (data) => {
-        socket.broadcast.emit('feedback', data)
-    })
+    socket.on("feedback", (data) => {
+        socket.broadcast.emit("feedback", data);
+    });
 }
-
-
